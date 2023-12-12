534ad98ff216f2d6e96dd8319995de3a3ad22f4b
KrillM <mks160615@gmail.com>
2023-12-12 10:23:32 +0900

    ✨ Feat :  서버 상 로그인 구현 성공

--- controller/CCrew.js
+++ controller/CCrew.js
@@ -54,8 +54,45 @@ exports.signUpProcess = async (req, res) => {
         }
 };
 
+exports.loginProcess = (req, res) => {
+    const { email, password } = req.body;
+
+    Crew.findOne({ where: { email: email } }).then((result) => {
+        console.log(result);
+        if (result) {
+            const compare = comparePassword(password, result.dataValues.password);
+            if (compare) {
+                console.log("로그인 성공");
+                req.session.user = email;
+                res.send(email);
+            } else if (!compare) {
+                res.status(400).send("비밀번호가 틀렸습니다.");
+            }
+        } else {
+            res.status(400).send("등록된 이메일이 없습니다.");
+        }
+    });
+}
+
+exports.logoutProcess = (req, res) => {
+    if(req.session.user){
+        req.session.destroy((err) => {
+            res.send({ result: true })
+        })
+    }
+    else{
+        res.send({ result: false })
+    }
+}
+
 exports.hashPassword = (password) => {
     const salt = crypto.randomBytes(16).toString("hex");
     const hash = crypto.pbkdf2Sync(password, salt, 1000, 64, "sha512").toString("hex");
     return `${salt}:${hash}`;
+}
+
+function comparePassword (enteredPassword, storedPassword) {
+    const [salt, originalHash] = storedPassword.split(":");
+    const hash = crypto.pbkdf2Sync(enteredPassword, salt, 1000, 64, "sha512").toString("hex");
+    return hash == originalHash;
 }
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const crew = require("../controller/CCrew")
 
-// 홈페이지
 router.get("/", crew.homePage);
 router.get("/signup", crew.signUpPage);
 router.post("/signup", crew.signUpProcess);
+
 router.get("/login", crew.loginPage);
+router.post("/login", crew.loginProcess);
 
 module.exports=router;
