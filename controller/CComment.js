cc435c582717f604e911222906bdfac6d1c4b6cc
KrillM <mks160615@gmail.com>
2023-12-22 16:00:17 +0900

    ✨ Feat :  댓글 작성 기능 성공과 디자인 수정

--- controller/CComment.js
+++ controller/CComment.js
@@ -1,16 +1,16 @@
 const { Comment } = require("../model");
 
 exports.createComment = (req, res) => {
-    const reviewId = req.params.reviewId;
+    const { comment, writtenBy, writtenAt } = req.body;
 
     Comment.create({
-        comment: req.params.comment,
-        crewId: req.params.crewId,
-        reviewId: reviewId
+        comment: comment,
+        writtenBy: writtenBy,
+        writtenAt: writtenAt
     }).then((result) => {
         res.json(result);
     }).catch((err) => {
         console.log(err);
         res.status(500).send("등록 오류 발생");
-    })
+    });
 }
